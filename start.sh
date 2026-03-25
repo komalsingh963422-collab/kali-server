f4cc981586324554d94c9e67fc5b59f98bc3fc9a
komalsingh963422-collab <komalsingh963422@gmail.com>
2026-03-26 01:22:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,16 @@ mkdir -p /root/.vnc
 echo "kali" | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 
+cat <<EOF > /root/.vnc/xstartup
+#!/bin/bash
+xrdb \$HOME/.Xresources
+startxfce4 &
+EOF
+
+chmod +x /root/.vnc/xstartup
+
 vncserver :1 -geometry 1280x720 -depth 24
+
+sleep 5
 
 websockify --web=/usr/share/novnc/ 8080 localhost:5901
